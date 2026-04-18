46b9bf718a54d5406d29fb969db831482bc479b5
Nikola <rile.kv@gmail.com>
2026-04-18 16:15:45 +0200

    feat: add AddVehicleDialog for manual vehicle entry

---
+++ src/AddVehicleDialog.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <QDialog>
+#include <QLineEdit>
+
+class AddVehicleDialog : public QDialog {
+    Q_OBJECT
+public:
+    explicit AddVehicleDialog(QWidget* parent = nullptr);
+
+private slots:
+    void onSubmit();
+
+private:
+    QLineEdit* m_owner;
+    QLineEdit* m_makeModel;
+    QLineEdit* m_chassisNumber;
+    QLineEdit* m_year;
+    QLineEdit* m_enginePowerKw;
+    QLineEdit* m_transmission;
+    QLineEdit* m_mileage;
+    QLineEdit* m_registration;
+    QLineEdit* m_color;
+};
